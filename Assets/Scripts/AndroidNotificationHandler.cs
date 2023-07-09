438fa8a5479f9ad477d850f1d7d84b39ded60211
GigaOrts <ortsortskhoev95@gmail.com>
2023-07-09 21:29:58 +0300

    ready project. IOS Android builds, notifications

---
+++ Assets/Scripts/AndroidNotificationHandler.cs
@@ -0,0 +1,36 @@
+using System;
+#if UNITY_ANDROID
+using Unity.Notifications.Android;
+#endif
+using UnityEngine;
+
+public class AndroidNotificationHandler : MonoBehaviour
+{
+#if UNITY_ANDROID
+    private static string ChannelId = "notification_channel";
+
+    public void ScheduleNotification(DateTime dateTime)
+    {
+        AndroidNotificationChannel notificationChannel = new AndroidNotificationChannel
+        {
+            Id = ChannelId,
+            Name = "Notification Channel",
+            Description = "random description",
+            Importance = Importance.Default
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(notificationChannel);
+
+        AndroidNotification notification = new AndroidNotification
+        {
+            Title = "Energy recharged!",
+            Text = "Your energy recharged, come back to play again!",
+            SmallIcon = "default",
+            LargeIcon = "default",
+            FireTime = dateTime
+        };
+
+        AndroidNotificationCenter.SendNotification(notification, ChannelId);
+    }
+#endif
+}
